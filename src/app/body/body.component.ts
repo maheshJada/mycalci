91c4d32a66bdd2412d1ba852cf5bac266f0c8f40
Mahesh Jada <mjada@innominds.com>
2021-11-17 19:14:03 +0530

    added templet angular project

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
+   allVal = [
+    { id: "operand", val: "7" },
+    { id: "operand", val: "8" },
+    { id: "operand", val: "9" },
+    { id: "operator", val: "/" },
+    { id: "operand", val: "4" },
+    { id: "operand", val: "5" },
+    { id: "operand", val: "6" },
+    { id: "operator", val: "*" },
+    { id: "operand", val: "1" },
+    { id: "operand", val: "2" },
+    { id: "operand", val: "3" },
+    { id: "operator", val: "-" },
+    { id: "operand", val: "0" },
+    { id: "operand", val: "." },
+    { id: "operator", val: "%" },
+    { id: "operator", val: "+" },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
